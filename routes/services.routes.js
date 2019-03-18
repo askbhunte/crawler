5f65072cee8612a16d60b39af3c993a9b1f81717
rakimsth <raktim@rumsan.com>
2019-03-18 17:34:43 +0545

    minor fixes after merging

--- routes/services.routes.js
+++ routes/services.routes.js
@@ -14,7 +14,7 @@ router.use("/bullion", bullionRouter);
 router.use("/news", newsRouter);
 router.use("/forex", forexRouter);
 router.use("/movie", movieRouter);
-router.use("/flight", flightRouter);
+//router.use("/flight", flightRouter);
 // router.use("/pollution", pollutionRouter);
 // router.use("/stock", stockRouter);
 router.use("/bullion", bullionRouter);
