9e2c10f6cb55e4627524d4d2ee479bd7b58fa607
rakimsth <raktim@rumsan.com>
2019-03-19 17:31:38 +0545

    horoscope file added

--- routes/services.routes.js
+++ routes/services.routes.js
@@ -8,12 +8,15 @@ const pollutionRouter = require("../services/pollution");
 const stockRouter = require("../services/stock");
 const bullionRouter = require("../services/bullion");
 const holidayRouter = require("../services/holiday");
+const horoscopeRouter = require("../services/horoscope");
 
 router.use("/holiday", holidayRouter);
 router.use("/bullion", bullionRouter);
+router.use("/horoscope", horoscopeRouter);
 router.use("/news", newsRouter);
 router.use("/forex", forexRouter);
 router.use("/movie", movieRouter);
+
 //router.use("/flight", flightRouter);
 // router.use("/pollution", pollutionRouter);
 // router.use("/stock", stockRouter);
