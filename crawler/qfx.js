e5d4dc9af59665ed43aef6a9c8ae71ecccfe9dbb
Dipesh Lohani <dipeshlohanii@gmail.com>
2019-07-19 15:15:45 +0545

    movie re-written

--- crawler/qfx.js
+++ crawler/qfx.js
@@ -7,24 +7,11 @@ const CrawlUtils = require("./utils");
 let baseUrl = "https://www.qfxcinemas.com/home";
 let apiUrl = "https://staging.qfxcinemas.com:3000/api/public/NowShowing";
 
-function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
 class QFX {
   constructor() {}
 
   async scrapMovieList() {
-    // let { data } = await axios.get(apiUrl);
-    let { data, ...res } = await axios.get(
-      "https://staging.qfxcinemas.com:2005/api/public/ThumbnailImage?eventId=11"
-    );
+    let { data } = await axios.get(apiUrl);
     return data.data;
   }
 
