2f43056d3c29b2f1ed86577b7583398d59ae9420
Santosh <santosh@rumsan.com>
2020-03-04 14:18:39 +0545

    fixed market and added tweet feature

--- crawler/market.js
+++ crawler/market.js
@@ -3,6 +3,8 @@ const cheerio = require("cheerio");
 const config = require("config");
 const moment = require("moment");
 const CrawlUtils = require("./utils");
+const fs = require("fs");
+var Twitter = require("twitter");
 
 let baseUrl = "http://www.nepalstock.com/";
 
@@ -14,11 +16,11 @@ class market {
     let indices = [];
     let sub_indices = [];
     //SUMMARY
-    let market_summary = $($("table")[8])
+    let market_summary = $("#market-watch > div.panel-body > table")
       .find("tbody")
       .find("td");
     //INDICES
-    $($("table")[9])
+    $("#nepse-stats > div:nth-child(3) > div.panel-body > table:nth-child(1)")
       .find("tbody")
       .find("tr")
       .each(function(i, elem) {
@@ -30,7 +32,7 @@ class market {
             .trim() !== ""
         )
           indices[i] = {
-            index: $(this)
+            name: $(this)
               .find("td")
               .eq(0)
               .text()
@@ -44,11 +46,22 @@ class market {
               .find("td")
               .eq(2)
               .text()
+              .trim(),
+            percent_change: $(this)
+              .find("td")
+              .eq(3)
+              .text()
+              .trim(),
+            status: $(this)
+              .find("img")
+              .attr("src")
               .trim()
+              .replace("./images/", "")
+              .replace(".gif", "")
           };
       });
     //SUB-INDICES
-    $($("table")[10])
+    $("#nepse-stats > div:nth-child(3) > div.panel-body > table:nth-child(2)")
       .find("tbody")
       .find("tr")
       .each(function(i, elem) {
@@ -60,7 +73,7 @@ class market {
             .trim() !== ""
         )
           sub_indices[i] = {
-            index: $(this)
+            name: $(this)
               .find("td")
               .eq(0)
               .text()
@@ -74,37 +87,90 @@ class market {
               .find("td")
               .eq(2)
               .text()
+              .trim(),
+            percent_change: $(this)
+              .find("td")
+              .eq(3)
+              .text()
+              .trim(),
+            status: $(this)
+              .find("img")
+              .attr("src")
               .trim()
+              .replace("./images/", "")
+              .replace(".gif", "")
           };
       });
+
+    fs.writeFileSync(
+      __dirname + "/../play/test.html",
+      $("#nepse-stats > div:nth-child(3) > div.panel-body > table:nth-child(2)")
+        .find("tbody")
+        .find("tr")
+    );
+
     market_summary = {
       state: $("#top-notice-bar")
         .text()
-        .trim()
-        .replace("Market ", ""),
-      total_turnover: market_summary.eq(1).text(),
-      traded_shares: market_summary.eq(3).text(),
-      total_transactions: market_summary.eq(5).text(),
-      total_scrips_traded: market_summary.eq(7).text(),
-      nepse: indices.find(d => d.index == "NEPSE"),
-      sensitive: indices.find(d => d.index == "Sensitive"),
-      float: indices.find(d => d.index == "Float"),
-      float_sensitive: indices.find(d => d.index == "Sen. Float"),
-      si_banking: sub_indices.find(d => d.index == "Banking"),
-      si_trading: sub_indices.find(d => d.index == "Trading"),
-      si_hotels: sub_indices.find(d => d.index == "Hotels"),
-      si_devbank: sub_indices.find(d => d.index == "Development Bank"),
-      si_hydro: sub_indices.find(d => d.index == "HydroPower"),
-      si_finance: sub_indices.find(d => d.index == "Finance"),
-      si_nonlife: sub_indices.find(d => d.index == "Non Life Insurance"),
-      si_manu: sub_indices.find(d => d.index == "Manu.& Pro."),
-      si_others: sub_indices.find(d => d.index == "Others"),
-      si_microfinance: sub_indices.find(d => d.index == "Microfinance"),
-      si_life: sub_indices.find(d => d.index == "Life Insurance"),
-      si_mutual: sub_indices.find(d => d.index == "Mutual Fund")
+        .trim(),
+      total_turnover: parseInt(
+        market_summary
+          .eq(1)
+          .text()
+          .replace(/,/g, "")
+      ),
+      traded_shares: parseInt(
+        market_summary
+          .eq(3)
+          .text()
+          .replace(/,/g, "")
+      ),
+      total_transactions: parseInt(
+        market_summary
+          .eq(5)
+          .text()
+          .replace(/,/g, "")
+      ),
+      total_scrips_traded: parseInt(
+        market_summary
+          .eq(7)
+          .text()
+          .replace(/,/g, "")
+      ),
+      nepse: this.getIndexData(indices, "NEPSE"),
+      sensitive: this.getIndexData(indices, "Sensitive"),
+      float: this.getIndexData(indices, "Float"),
+      float_sensitive: this.getIndexData(indices, "Sen. Float"),
+      si_banking: this.getIndexData(sub_indices, "Banking"),
+      si_trading: this.getIndexData(sub_indices, "Trading"),
+      si_hotels: this.getIndexData(sub_indices, "Hotels"),
+      si_devbank: this.getIndexData(sub_indices, "Development Bank"),
+      si_hydro: this.getIndexData(sub_indices, "HydroPower"),
+      si_finance: this.getIndexData(sub_indices, "Finance"),
+      si_nonlife: this.getIndexData(sub_indices, "Non Life Insurance"),
+      si_manu: this.getIndexData(sub_indices, "Manu.& Pro."),
+      si_others: this.getIndexData(sub_indices, "Others"),
+      si_microfinance: this.getIndexData(sub_indices, "Microfinance"),
+      si_life: this.getIndexData(sub_indices, "Life Insurance"),
+      si_mutual: this.getIndexData(sub_indices, "Mutual Fund")
     };
     return market_summary;
   }
+
+  getIndexData(index, name) {
+    let data = index.find(d => d.name == name);
+    try {
+      data.value = parseFloat(data.value.replace(/,/g, ""));
+      data.change = parseFloat(data.change.replace(/,/g, ""));
+      data.percent_change = parseFloat(data.percent_change);
+      if (data.status == "decrease") {
+        data.change = data.change * -1;
+        data.percent_change = data.percent_change * -1;
+      }
+    } catch (e) {}
+    return data;
+  }
+
   async process() {
     let details = await this.scrape();
 
@@ -116,6 +182,31 @@ class market {
       }
     });
 
+    return data;
+  }
+
+  async tweet() {
+    var client = new Twitter(config.get("services.twitter"));
+
+    let data = await this.scrape();
+    let indicator = "🟢";
+    let plus = "+";
+    if (data.nepse.change < 0) {
+      indicator = "🔴";
+      plus = "";
+    }
+
+    let status = `NEPSE:     ${data.nepse.value} (${plus}${data.nepse.change}/${plus}${data.nepse.percent_change}%)
+Sensitive:   ${data.sensitive.value} (${plus}${data.sensitive.change}/${plus}${data.sensitive.percent_change}%) 
+    
+${indicator} #nepse #nepal #stock #nepalstock #market`;
+
+    client.post("statuses/update", { status }, function(error, tweets, response) {
+      if (!error) {
+      } else {
+        console.log(error);
+      }
+    });
     return data;
   }
 }
