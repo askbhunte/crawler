dc0954ee0404eecc49c0bf0073078a73254c4043
rakimsth <raktim@rumsan.com>
2020-04-30 20:47:01 +0545

    forex fix

--- crawler/forex.js
+++ crawler/forex.js
@@ -1,17 +1,39 @@
 const axios = require("axios");
 const config = require("config");
-var Twitter = require("twitter");
+let Twitter = require("twitter");
 const utils = require("./utils");
+const cheerio = require("cheerio");
 
 const fs = require("fs");
 let dataFile = __dirname + "/../config/data.json";
 
-let baseUrl = "https://nrb.org.np/exportForexJSON.php";
+let baseUrl = "http://www.nrb.org.np/forex";
 
 class Forex {
   async scrapeForex() {
     let { data } = await axios.get(baseUrl);
-    return data.Conversion.Currency;
+    const $ = cheerio.load(data);
+    let retData = [];
+    $(".table-forex")
+      .find("tr")
+      .each(function (i, elem) {
+        retData.push({
+          base_currency: $(this)
+            .find(".d-flex")
+            .find(".ml-2")
+            .text()
+            .split(" ")[0]
+            .trim()
+            .toUpperCase(),
+          target_currency: "NPR",
+          buy: $(this).find("td:nth-child(3)").text(),
+          base_value: $(this).find("td:nth-child(2)").text(),
+          sell: $(this).find("td:nth-child(4)").text(),
+          date: new Date()
+        });
+      });
+    retData = retData.filter(d => d.base_currency);
+    return retData;
   }
   async process() {
     let forexData = await this.scrapeForex();
@@ -38,13 +60,13 @@ class Forex {
     var client = new Twitter(config.get("services.twitter"));
 
     let status = `1 USD = NRs: ${data.USD + utils.getChangeEmoji(data.USD, prevData.forex.USD)}
-1 AUD = NRs. ${data.AUD + utils.getChangeEmoji(data.AUD, prevData.forex.AUD)}
-1 EUR =  NRs. ${data.EUR + utils.getChangeEmoji(data.EUR, prevData.forex.EUR)}
-1 GBP =  NRs. ${data.GBP + utils.getChangeEmoji(data.GBP, prevData.forex.GBP)}
-    
-#dollar #rate #nepal #forex #currency #usd #aud #nepali #rupee`;
+  1 AUD = NRs. ${data.AUD + utils.getChangeEmoji(data.AUD, prevData.forex.AUD)}
+  1 EUR =  NRs. ${data.EUR + utils.getChangeEmoji(data.EUR, prevData.forex.EUR)}
+  1 GBP =  NRs. ${data.GBP + utils.getChangeEmoji(data.GBP, prevData.forex.GBP)}
 
-    client.post("statuses/update", { status }, function(error, tweets, response) {
+  #dollar #rate #nepal #forex #currency #usd #aud #nepali #rupee`;
+
+    client.post("statuses/update", { status }, function (error, tweets, response) {
       if (!error) {
         prevData.forex = data;
         fs.writeFileSync(dataFile, JSON.stringify(prevData));
